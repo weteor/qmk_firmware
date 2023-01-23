e66d73ec7badbe924a531fe919c119735c1ac563
Ross <rmontsinger@gmail.com>
2023-01-23 16:09:17 -0500

    it works!

--- keyboards/montsinger/fern/config.h
+++ keyboards/montsinger/fern/config.h
@@ -5,32 +5,27 @@
 
 #include "config_common.h"
 
-#define VENDOR_ID 0x1234
-#define PRODUCT_ID 0x5678
-#define DEVICE_VER 0x0001
-
-
 /* key matrix size */
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 6
 
 #define DIODE_DIRECTION ROW2COL
 
-/*
+
 #define MATRIX_ROW_PINS { GP28, GP20, GP2, GP5, GP9, GP29, GP22, GP3, GP4, GP8 }
 #define MATRIX_COL_PINS { GP1, GP0, GP26, GP27, GP21, GP23 }
 
 
 #define ENCODERS_PAD_A { GP7 }
 #define ENCODERS_PAD_B { GP6 }
-*/
 
+/*
 #define MATRIX_ROW_PINS { F5, B3, D1, C6, B5, F4, B1, D0, D4, B4 }
 #define MATRIX_COL_PINS { D2, D3, F7, F6, B6, B2 }
 
 #define ENCODERS_PAD_A { D7 }
 #define ENCODERS_PAD_B { E6 }
-
+*/
 /* #define RGB_DI_PIN GP12 */
 
 
