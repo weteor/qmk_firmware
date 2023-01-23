a468320301bb9143d2447d5a4a6c1b6dd9b60a0f
Ross <rmontsinger@gmail.com>
2023-01-23 16:08:46 -0500

    added rev4pi from ongoing master pull request

--- keyboards/montsinger/rebound/rev4pi/config.h
+++ keyboards/montsinger/rebound/rev4pi/config.h
@@ -17,15 +17,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* key matrix size */
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 7
 #define DIODE_DIRECTION ROW2COL
 
 #define MATRIX_ROW_PINS { GP2, GP3, GP4, GP5, GP26, GP27, GP28, GP29 }
 #define MATRIX_COL_PINS { GP6, GP7, GP8, GP9, GP23, GP20, GP22 }
 
-#define ENCODERS_PAD_A { GP1 }
-#define ENCODERS_PAD_B { GP0 }
+#define ENCODERS_PAD_A { GP0 }
+#define ENCODERS_PAD_B { GP1 }
 
 #define RGB_DI_PIN GP21
 
@@ -36,9 +34,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-/* Bootmagic Lite key configuration */
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 0
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
--- keyboards/montsinger/rebound/rev4pi/keymaps/rossman360/keymap.c
+++ keyboards/montsinger/rebound/rev4pi/keymaps/rossman360/keymap.c
@@ -1,4 +1,18 @@
-#include QMK_KEYBOARD_H
+/* Copyright 2023 Ross Montsinger (@rossman360)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */#include QMK_KEYBOARD_H
 #include "rossman360.h"
 
 #define PGMOD LT(_NUM, KC_PGDN)
@@ -54,17 +68,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, KC_0   , _______, _______, KC_NO  , KC_NO  , KC_0   , _______, _______, _______
   )
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-
-    if (clockwise) {
-      tap_code16(KC_VOLD);
-    } else {
-      tap_code16(KC_VOLU);
-    }
-
-    return true;
-}
 
 #ifdef OLED_ENABLE
 bool oled_task_user(void) {
