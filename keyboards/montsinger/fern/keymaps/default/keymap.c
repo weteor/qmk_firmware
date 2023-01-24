bcfb56f6b373374a0ee7413be593dace3ccd84f3
Ross <rmontsinger@gmail.com>
2023-01-23 19:21:00 -0500

    blank layer

--- keyboards/montsinger/fern/keymaps/default/keymap.c
+++ keyboards/montsinger/fern/keymaps/default/keymap.c
@@ -6,7 +6,8 @@
 // entirely and just use numbers.
 
 enum layer_names {
-  _QWERTY
+  _QWERTY,
+  _BLANK
 };
 
 enum custom_keycodes {
@@ -19,10 +20,18 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
-   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,            KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   QK_BOOT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
    KC_BSPC, KC_2,    KC_3,    KC_LCTL, KC_TAB,  KC_PGDN,         KC_ENT,  KC_SPC,  KC_1,    KC_2,    KC_3,    KC_4
+),
+[_BLANK] = LAYOUT(
+      _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, 
+     _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______,
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______
 )
+
 };
