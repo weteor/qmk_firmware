30163f14cc77a2c03cca0dba6d2ac61008446aef
Ross <rmontsinger@gmail.com>
2023-01-27 14:01:56 -0500

    added blank layer

--- keyboards/montsinger/shrub/keymaps/rossman360/keymap.c
+++ keyboards/montsinger/shrub/keymaps/rossman360/keymap.c
@@ -34,15 +34,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_BLANK] = LAYOUT(
-   KC_ESC,                                                                                                              QK_BOOT,
-            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,   KC_BSPC, 
-   KC_TAB,                                                                                                              KC_NO,
-             KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          
-   KC_CAPS,
-              ZSHIFT,  KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,         KC_UP,
-   KC_4,
-               KC_BSPC, KC_LCTL, XXXXXXX, XXXXXXX, KC_SPC, XXXXXXX, XXXXXXX, KC_MINS,                  
-   KC_5,                                                                                              KC_LEFT, KC_DOWN, KC_RIGHT
+   _______,                                                                                                              _______,
+            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+   _______,                                                                                                              _______,
+             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          
+   _______,
+              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______,
+   _______,
+               _______, _______, _______, _______, _______, _______, _______, _______,                  
+   _______,                                                                                               _______, _______, _______
 )
 };
 
